fab994262ccc13baed11e6549737bf0aa76fb923
Earts-vinay <vinay.r@entitledarts.com>
2025-02-27 11:45:28 +0530

    changes on login token

--- src/redux/apiResponse/loginApiSlice.js
+++ src/redux/apiResponse/loginApiSlice.js
@@ -1,7 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const loginApiSlice = createSlice({
-  name: 'loginApi',
+  name: "loginApi",
   initialState: { token: null, user: null, error: null },
   reducers: {
     setLoginApiResponse: (state, action) => {
@@ -11,25 +11,33 @@ export const loginApiSlice = createSlice({
         state.user = data.user;
         state.expire = data.expire;
         state.error = null;
+
+        // Store token in localStorage as user_token
+        localStorage.setItem("user_token", data.token);
       } else {
         state.token = null;
         state.user = null;
         state.error = msg;
+
+        // Remove token from localStorage on failure
+        localStorage.removeItem("user_token");
       }
     },
     clearTokenAndUser: (state) => {
       state.token = null;
       state.user = null;
       state.error = null;
-    }
+
+      // Remove token from localStorage when logging out
+      localStorage.removeItem("user_token");
+    },
   },
 });
 
 export const { setLoginApiResponse, clearTokenAndUser } = loginApiSlice.actions;
 
 export const selectLoginApiResponse = (state) => state.loginApi;
-export const selectToken = (state) => state.loginApi.token;
+export const selectToken = () => localStorage.getItem('user_token');
 export const getTokenExpiry = (state) => state.loginApi.expire;
-
 
 export default loginApiSlice.reducer;
